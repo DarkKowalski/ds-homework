d3abe711f99289c7ebb2e5d6de8a3e9a17861629
kowalski <darkkowalski2012@gmail.com>
2020-10-17 23:31:49 +0800

    fix blocking issue

--- src/hw2/lib/rush/controller.rb
+++ src/hw2/lib/rush/controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'socket'
+require 'timeout'
+
 module Rush
   class Controller
     attr_reader :nodes, :sockets
@@ -20,28 +22,41 @@ module Rush
       nil
     end
 
+    def probe_socket(socket)
+      @logger.debug("Probe socket #{socket}")
+
+      node_uuid = nil
+      begin
+        Timeout.timeout(10) do
+          socket.puts 'probe'
+          node_uuid = socket.gets&.chomp
+        end
+      rescue Timeout::Error => e
+        @logger.warn("Failed to probe #{socket}")
+        @logger.error(e.message.to_s)
+        return nil
+      end
+
+      node_uuid
+    end
+
     def probe_node(ip, port)
       @logger.debug("Probe Node #{ip}:#{port}")
 
       socket = create_socket(ip, port)
       return if socket.nil?
 
-      socket.puts 'probe'
-      node_uuid = socket.gets&.chomp
-
-      if node_uuid.nil?
-        @logger.warn("Failed to probe #{ip}:#{port}")
-        return nil
-      end
-
+      uuid = probe_socket(socket)
       socket.close
-      node_uuid
+
+      uuid
     end
 
     def add_node(ip, port)
-      uuid = probe_node(ip, port).to_s
+      uuid = probe_node(ip, port)
       return if uuid.nil?
 
+      uuid = uuid.to_s
       if @nodes.key?(uuid)
         @logger.warn("Duplicated Node #{uuid}")
         return
@@ -62,14 +77,14 @@ module Rush
 
     def connect_node(uuid)
       uuid = uuid.to_s
-      return unless @nodes.key?(uuid)
+      return unless @nodes.key?(uuid) # Non-exsiting node
 
       ip, port = @nodes[uuid]
       socket = create_socket(ip, port)
       return if socket.nil?
 
       @sockets[uuid] = socket
-      @logger.debug("Connect to Node #{uuid}, ip = #{ip}, port = #{port}")
+      @logger.debug("Connect to Node #{uuid}, ip = #{ip}, port = #{port} #{socket}")
     end
 
     def disconnect_node(uuid)
@@ -77,8 +92,8 @@ module Rush
       return unless @sockets.key?(uuid)
 
       @sockets[uuid].close
-      @sockets.delete(uuid)
-      @logger.debug("Disconnect from Node #{uuid}")
+      socket = @sockets.delete(uuid)
+      @logger.debug("Disconnect from Node #{uuid}, #{socket}")
     end
 
     # Connect to all nodes
@@ -89,6 +104,18 @@ module Rush
     # Disconnect from all nodes
     def disconnect
       @nodes.each_key { |uuid| disconnect_node(uuid) }
+    end
+
+    # Probe all nodes
+    def probe
+      @nodes.each do |uuid, ip_port|
+        uuid = uuid.to_s
+        if @sockets.key?(uuid)
+          @logger.warn("Existing connection to Node #{uuid} #{@sockets[uuid]}")
+          next
+        end
+        probe_node(*ip_port)
+      end
     end
   end
 end
